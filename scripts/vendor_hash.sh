f567d9b99bc17fc7b8cc1307545fc2aeaafe4584
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-09-06 23:31:41 +0100

    finally fix the update script

--- scripts/vendor_hash.sh
+++ scripts/vendor_hash.sh
@@ -2,18 +2,16 @@
 
 # inspired by https://github.com/ghostty-org/ghostty/blob/main/nix/build-support/check-zig-cache-hash.sh
 
-set -ex
+set -e
 VENDOR_HASH_FILE=$(realpath "$(dirname "$0")/../nix/goVendorHash.nix")
 
 CURRENT_HASH=$(nix eval --raw --file "$VENDOR_HASH_FILE")
-echo "Current hash: $CURRENT_HASH"
 
 GO_VENDOR_DIR="$(mktemp --directory --suffix=gophertype-vendor)"
 
 go mod vendor -o "$GO_VENDOR_DIR"
 
 VENDOR_HASH=$(nix hash path "$GO_VENDOR_DIR")
-echo "New hash: $VENDOR_HASH"
 
 if [ "$CURRENT_HASH" == "$VENDOR_HASH" ]; then
   echo "Go vendor hash is up to date"
